2434912d5b7cf756f3c3820d679b5bdf388bc456
Alvaro cádiz <73478720+alvarocadizdev@users.noreply.github.com>
2025-04-29 19:07:50 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
-
+//prueba1
 const app = express();
 app.use(cors());
 app.use(express.json());
